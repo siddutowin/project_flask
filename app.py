7885c9b63e969c1c7feb37ce38282bc7702dbcac
bharath41454 <43461946+bharath41454@users.noreply.github.com>
2019-12-06 10:46:13 +0530

    test

---
+++ app.py
@@ -0,0 +1,115 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+  'projectId': 'demo12-d4cf8',
+})
+
+db = firestore.client()
+
+
+@app.route('/')
+def hello_world():
+    print(db)
+    return "hello"
+
+
+# @app.route('/insert')
+# def add_data_one():
+#     db = firestore.Client()
+#     doc_ref = db.collection(u'users').document(u'all')
+#     doc_ref.set({
+#         u'username': u'siddesha',
+#         u'password': u'123456',
+#         u'born': 1987
+#     })
+#     return 'inserted'
+
+
+# @app.route('/read')
+# def get_collection():
+#     db = firestore.Client()
+#     # [START quickstart_get_collection]
+#     users_ref = db.collection(u'users')
+#     docs = users_ref.stream()
+#
+#     for doc in docs:
+#         print(u'{} => {}'.format(doc.id, doc.to_dict()))
+#     return 'reading data'
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.json['username']
+    password = request.json['password']
+    rpassword = request.json['rpassword']
+    users = {
+        username:'username',
+        password:'password',
+        rpassword:'rpassword'
+
+    }
+    return users
+
+
+@app.route('/send', methods=['POST'])
+def send():
+    db = firestore.Client()
+    doc_ref = db.collection(u'users').document(u'all')
+    user = request.json['username']
+    pasw = request.json['password']
+    passr = request.json['rpassword']
+    # return (user)
+    # return (pasw)
+    if pasw == passr:
+       resp = doc_ref.set({
+         u'username': user,
+         u'password': pasw,
+    })
+    else:
+       return "Please Try again"
+    if resp:
+        return "Inserted"
+    else:
+        return "Try again"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
